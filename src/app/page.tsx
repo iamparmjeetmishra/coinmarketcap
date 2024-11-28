d1365b1cd8723c498699ab2debb54c39470ca376
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-28 16:07:16 +0530

    Button fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-2">
       HomePage Here we are going to add Marketing sections
-      <Link href="/dashboard">Go to dashboard</Link>
+      <Button>
+        <Link href="/dashboard">Go to dashboard</Link>
+      </Button>
     </div>
   );
 }
