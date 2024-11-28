25b2861a609f4408967a5bcc7ed3128284c0f502
Parmjeet <iamparmjeetmishra@gmail.com>
2024-11-28 16:01:21 +0530

    Crypto app init

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   experimental: {
+      typedRoutes: true
+   },
+   crossOrigin: "anonymous",
+};
+
+export default nextConfig;
